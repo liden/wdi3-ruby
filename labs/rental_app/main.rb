2c7a189ebbcf84001691532b367b76975ea93342
lara iden <lara.iden@gmail.com>
2013-02-01 14:02:40 -0500

    lectures + database.txt

---
+++ labs/rental_app/main.rb
@@ -0,0 +1,36 @@
+require 'pry'
+require 'rainbow'
+require_relative 'building'
+require_relative 'apartment'
+require_relative 'person'
+
+building = nil
+
+print "add (p)erson or (a)partment or (b)uilding or (q)uit: "
+response = gets.chomp.downcase
+while response != 'q'
+  if response == 'p'
+    print "name: "
+    name = gets.chomp
+    print "age: "
+    age = gets.to_i
+    print "gender: "
+    gender = gets.chomp
+    print "what apartment do you want to live in: "
+    apt = gets.chomp
+    building.apartments[apt].renters << Person.new(name, age, gender)
+  elsif response == 'a'
+    print "name: "
+    name = gets.chomp
+    print "sq ft: "
+    sqft = gets.to_i
+    building.apartments[name] = Apartment.new(name, sqft, 0, 0)
+  elsif response == 'b'
+    building = Building.new('10E21ST', 'GA', false, false, 10)
+  end
+
+  print "add (p)erson or (a)partment or (b)uilding or (q)uit: "
+  response = gets.chomp.downcase
+end
+
+binding.pry
